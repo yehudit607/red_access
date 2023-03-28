524298e14a18caf8951cd1a382c4aa5c61be3174
YehuditPayEm <102038702+YehuditPayEm@users.noreply.github.com>
2023-03-29 02:43:55 +0300

    redis

--- antivirus/antivirus_api/tasks.py
+++ antivirus/antivirus_api/tasks.py
@@ -1,16 +1,17 @@
 from celery import shared_task
 import requests
+from rest_framework import status
 
 from antivirus.antivirus import settings
+from antivirus.redis import RedisWords
 
-MALICIOUS_WORDS = []
-
+redis_words = RedisWords()
 
 @shared_task
 def fetch_configuration_data(version):
     response = requests.get(f"http://localhost/api/config?version={version}&company_id={settings.COMPANY_ID}")
-    if response.status_code == 200:
+    if response.status_code == status.HTTP_200_OK:
         data = response.json()
         if data.get('malicious_words'):
-            global MALICIOUS_WORDS
-            MALICIOUS_WORDS = data['malicious_words']
+            redis_words.set_malicious_words(data['malicious_words'])
+            redis_words.set_version(data['version'])
--- antivirus/redis/__init__.py
+++ antivirus/redis/__init__.py
@@ -1,15 +1,31 @@
 import redis
 
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+DB = 0
+CHARSET = "utf-8"
+DECODE_RESPONSE = True
 
 class RedisWords:
     def __new__(cls):
         if not hasattr(cls, "instance"):
             cls.instance = redis.StrictRedis(
-                host="localhost",
-                port=6379,
-                db=0,
-                charset="utf-8",
-                decode_responses=True,
+                host=REDIS_HOST,
+                port=REDIS_PORT,
+                db=DB,
+                charset=CHARSET,
+                decode_responses=DECODE_RESPONSE,
             )
         return cls.instance
 
+    def set_malicious_words(self, words):
+        self.instance.set("malicious_words", words)
+
+    def get_malicious_words(self):
+        return self.instance.get("malicious_words")
+
+    def set_version(self, version):
+        self.instance.set("version", version)
+
+    def get_version(self):
+        return self.instance.get("version")
